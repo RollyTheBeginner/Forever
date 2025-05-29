3f833436c20283b0ba3e5c0e751f5abe176f438e
Rolly G. Jr Carrascal <rgcarrascal@bluesky.com.ph>
2025-05-29 16:45:47 +0800

    Initial commit

---
+++ my-first-react-app/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}"  // Important: add this so Tailwind scans your files for classes
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
